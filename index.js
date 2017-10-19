71199d0c22d1dab765176779db345b8016451729
Ramesh <gurams@gmail.com>
2017-10-19 04:58:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ function makeInt(n){
 function preserveDecimal(n){
   return parseFloat(n);
 }
+function add(a,b){
+  return parseInt(a)+parseInt(b);
+}
+function subtract(a,b){
+  return parseInt(a)-parseInt(b);
+}
+function multiply(a,b){
+  return parseInt(a)*parseInt(b);    
+}
+function divide(a,b){
+  return parseInt(a)/parseInt(b);    
+}
+function inc(n){
+  return parseInt(++n);
+}
+function inc(n){
+  return parseInt(--n);
+}
